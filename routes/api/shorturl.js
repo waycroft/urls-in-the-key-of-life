76d5793f711794be809e999c2b60a4a044e3a522
Tyler Termini <tntermini@gmail.com>
2021-12-09 11:54:38 -0800

    successful error messages for invalid URLs

--- routes/api/shorturl.js
+++ routes/api/shorturl.js
@@ -6,11 +6,15 @@ var fsPromises = require('fs/promises');
 var random = require(process.cwd() + '/src/generateRandomNum');
 const songList = './data/song-list.json';
 
+let originalUrl;
+let randomSong;
+
 // actually make it go the the URL
 
 async function getRandomSong() {
     let songList = await getSongList();
     let index = random.generateRandomInRange(songList.length, 0);
+
     return String(songList[index]).toLocaleLowerCase();
 }
 
@@ -20,12 +24,31 @@ async function getSongList() {
     return JSON.parse(data);
 } 
 
+function isValidUrl(string) {
+    let match = string.match(/https?:\/\/[A-Za-z]+\.[A-Za-z]+/g);
+    console.log(match);
+    return !!match;
+}
+
 router.get('/', async (req, res, next) => {
     res.send(await getRandomSong());
 })
 
 router.post('/', urlencodedParser, async (req, res, next) => {
-    res.send({url: req.body.url, json: await getRandomSong()});
+    originalUrl = req.body.url;
+
+    if (!isValidUrl(originalUrl)) {
+        res.json({error: 'invalid url'}); 
+        return;
+    };
+
+    randomSong = await getRandomSong();
+    res.send({original_url: req.body.url, short_url: randomSong});
+})
+
+router.get('/songName', (req, res, next) => {
+    if (!randomSong) throw 'no url generated';
+    res.redirect(originalUrl);
 })
 
 module.exports = router;
